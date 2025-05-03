6984a343ddd2ffbb4cd94bef1fee63adc302eacb
berzz26 <aumtamboli2233@gmail.com>
2025-05-04 01:13:00 +0530

    created functions to detect and read env files in root dir

--- extension.js
+++ extension.js
@@ -1,6 +1,9 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+
 const vscode = require('vscode');
+const fs = require('fs')
+const os = require('os')
+const path = require('path');
+
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -12,23 +15,80 @@ function activate(context) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "env-vault" is now active!');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
+
+	const workspaceFolder = vscode.workspace.workspaceFolders?.[0].uri.fsPath;
+	const getEnvFiles = () => {
+		const files = fs.readdirSync(workspaceFolder); // List all files in the workspace
+
+		// Filter out the files that start with `.env`
+		const envFiles = files.filter(file => file.startsWith('.env'));
+
+		return envFiles;
+	};
+
+	const readEnv = (filename) => {
+		
+		fs.readFile(`${workspaceFolder}/${filename}`, 'utf8', (err, data) => {
+			if (err) {
+				vscode.window.showInformationMessage(''+err);
+				return;
+			}
+
+			vscode.window.showInformationMessage('File Content: ' + data);
+		});
+	};
+
+
+	const selectEnvFile = async () => {
+		const envFiles = getEnvFiles(); // Get the .env files
+
+		if (envFiles.length === 0) {
+			vscode.window.showInformationMessage('No .env files found in the workspace');
+			return;
+		}
+
+		const selectedFile = await vscode.window.showQuickPick(envFiles, {
+			placeHolder: 'Select an .env file to work with'
+		});
+
+		if (!selectedFile) return;
+
+		// Proceed with the selected .env file
+		vscode.window.showInformationMessage(`Selected file: ${selectedFile}`);
+		console.log('Selected file:', selectedFile);
+
+		readEnv(selectedFile)
+	};
+
+
 	const disposable = vscode.commands.registerCommand('env-vault.helloWorld', function () {
 		// The code you place here will be executed every time your command is executed
+		console.log('Congratulations, your extension "env-vault" is now active!');
 
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from env_vault!');
 	});
+	const get = vscode.commands.registerCommand('env-vault.getEnv', function () {
+		// The code you place here will be executed every time your command is executed
+		selectEnvFile()
 
-	context.subscriptions.push(disposable);
+
+	});
+
+	const updateEnv = () => {
+
+
+	}
+
+	context.subscriptions.push(disposable, get);
 }
 
 // This method is called when your extension is deactivated
-function deactivate() {}
+function deactivate() { }
 
 module.exports = {
 	activate,
